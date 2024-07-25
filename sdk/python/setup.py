bb995e961ed1aeab881ba378c8de6e5a970581a1
Ryan <8829107+ryan-pip@users.noreply.github.com>
2024-07-25 12:23:44 +1200

    build: python sdk

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -9,7 +9,7 @@ from setuptools.command.install import install
 from subprocess import check_call
 
 
-VERSION = os.getenv("PULUMI_PYTHON_VERSION", "0.0.0")
+VERSION = os.getenv("PULUMI_PYTHON_VERSION", "0.0.8")
 def readme():
     try:
         with open('README.md', encoding='utf-8') as f:
