3ed5203d6891bf2593c54b69002245eff76688bd
ryan-pip <runner@fv-az665-16.fog2pefswkletp0y445k1dqqob.bx.internal.cloudapp.net>
2024-10-23 05:07:20 +0000

    make build_sdks

--- sdk/go/fivetran/getGroups.go
+++ sdk/go/fivetran/getGroups.go
@@ -27,7 +27,7 @@ import (
 //
 //	func main() {
 //		pulumi.Run(func(ctx *pulumi.Context) error {
-//			_, err := fivetran.GetGroups(ctx, nil, nil)
+//			_, err := fivetran.GetGroups(ctx, &fivetran.GetGroupsArgs{}, nil)
 //			if err != nil {
 //				return err
 //			}
