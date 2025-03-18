9968a4b90185b69519fbde6c4b8dd12427acccfd
Ryan <8829107+ryan-pip@users.noreply.github.com>
2025-03-19 08:23:45 +1300

    make build_sdks

--- sdk/go/fivetran/dbtTransformation.go
+++ sdk/go/fivetran/dbtTransformation.go
@@ -12,7 +12,7 @@ import (
 	"github.com/ryan-pip/pulumi-fivetran/sdk/go/fivetran/internal"
 )
 
-// Resource is in ALPHA state.
+// This resource is Deprecated, please follow the 1.5.0 migration guide to update the schema.
 //
 // This resource allows you to add, manage and delete dbt Transformations for existing dbt Model.
 // To retrieve available dbt Models use this [Retrieve dbt Project models](https://fivetran.com/docs/rest-api/dbt-transformation-management#retrievedbtprojectmodels) endpoint.
